07e1a49ed990f7083798a1948b40524657bc3de9
shogassxzp <rssxzp@icloud.com>
2025-11-01 20:31:51 +0300

    initial commit

---
+++ travelPass/travelPass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  travelPass
+//
+//  Created by Игнат Рогачевич on 1.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ travelPass/travelPass/travelPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelPassApp.swift
+//  travelPass
+//
+//  Created by Игнат Рогачевич on 1.11.25.
+//
+
+import SwiftUI
+
+@main
+struct travelPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
