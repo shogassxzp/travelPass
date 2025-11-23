cefa74ffb6303e99fde7b02738bd5c5edd9fe776
shogassxzp <rssxzp@icloud.com>
2025-11-23 09:41:48 +0500

    feat: реализован поиск и состояние с не найденым городом в citypicker

--- travelPass/travelPass/Screens/CityPickerScreen.swift
+++ travelPass/travelPass/Screens/CityPickerScreen.swift
@@ -10,12 +10,20 @@ import SwiftUI
 struct CityPickerScreen: View {
     @State var searchString: String = ""
     @State var cities = ["Москва", "Санкт-Петербург", "Сочи", "Краснодар"]
-    
+
+    var filteredCities: [String] {
+        if searchString.isEmpty {
+            return cities
+        } else {
+            return cities.filter { $0.localizedCaseInsensitiveContains(searchString) }
+        }
+    }
+
     var body: some View {
         VStack {
             TextField("Введите запрос", text: $searchString)
                 .textFieldStyle(.plain)
-                .font(.system(size: 17,weight: .regular))
+                .font(.system(size: 17, weight: .regular))
                 .padding(.horizontal, 30)
                 .frame(height: 36)
                 .background(.yLightGray)
@@ -25,11 +33,11 @@ struct CityPickerScreen: View {
                         Image(systemName: "magnifyingglass")
                             .foregroundStyle(.yGray)
                             .padding(.leading, 8)
-                        
+
                         Spacer()
-                        
+
                         if !searchString.isEmpty {
-                            Button(action:clearTextField) {
+                            Button(action: clearTextField) {
                                 Image(systemName: "xmark.circle.fill")
                                     .foregroundStyle(.yGray)
                                     .padding(.trailing, 8)
@@ -38,38 +46,50 @@ struct CityPickerScreen: View {
                     }
                 )
                 .padding(.horizontal, 16)
-                
         }
-        
-        
-        
+
         ScrollView {
-            LazyVStack(spacing: 0) {
-                ForEach(cities, id: \.self) { city in
-                    HStack{
-                        Text(city)
-                            .foregroundStyle(.primary)
-                            .font(.system(size: 16,weight: .regular))
-                        
-                        Spacer()
-                        
-                        Image(systemName: "chevron.right")
-                            .foregroundStyle(.yBlack)
-                            .font(.system(size: 14))
+            if filteredCities.isEmpty {
+                VStack {
+                    Spacer()
+
+                    Text("Город не найден")
+                        .foregroundStyle(.yBlack)
+                        .font(.system(size: 24, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.vertical, 40)
+
+                    Spacer()
+                }
+                .frame(height: UIScreen.main.bounds.height * 0.8)
+            } else {
+                LazyVStack(spacing: 0) {
+                    ForEach(filteredCities, id: \.self) { city in
+                        HStack {
+                            Text(city)
+                                .foregroundStyle(.primary)
+                                .font(.system(size: 16, weight: .regular))
+                                .padding(.leading, 24) // Базовые 16 + 8 оверлей
+
+                            Spacer()
+
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(.yBlack)
+                                .font(.system(size: 14))
+                                .padding(.trailing, 24) // Базовые 16 + 8 оверлей
+                        }
+
+                        .padding(.vertical, 12)
                     }
-                    .padding(.horizontal,16)
-                    .padding(.vertical, 12)
-                    
                 }
             }
-            
         }
     }
+
     private func clearTextField() {
         searchString = ""
     }
 }
-
 
 #Preview {
     CityPickerScreen()
