8123c53748b3176286d956f9bd2b8808de032462
shogassxzp <rssxzp@icloud.com>
2025-12-04 17:35:43 +0500

    fix: исправлен текст на превью сторис

--- travelPass/travelPass/Stories/StoryCell.swift
+++ travelPass/travelPass/Stories/StoryCell.swift
@@ -5,32 +5,41 @@ struct StoryCell: View {
     let width: CGFloat = 90
     let height: CGFloat = 140
     var body: some View {
-        VStack(spacing: 8) {
-            ZStack {
-                RoundedRectangle(cornerRadius: 16)
-                    .stroke(
-                        story.isViewed ? Color.clear : Color.blue,
-                        lineWidth: 4
-                    )
-                    .frame(width: width, height: height)
-                    .cornerRadius(16)
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(
+                    story.isViewed ? Color.clear : Color.blue,
+                    lineWidth: 8
+                )
+                .frame(width: width, height: height)
+                .cornerRadius(16)
 
-                Image(story.backgroundImage)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: width - 4, height: height - 4)
-                    .cornerRadius(14)
-                    .overlay {
-                        if story.isViewed {
-                            Color.gray.opacity(0.5)
-                                .cornerRadius(16)
-                        }
+            Image(story.backgroundImage)
+                .resizable()
+                .scaledToFill()
+                .frame(width: width - 4, height: height - 4)
+                .cornerRadius(14)
+                .overlay {
+                    if story.isViewed {
+                        Color.gray.opacity(0.5)
+                            .cornerRadius(16)
                     }
+                }
+
+            VStack {
+                Spacer()
+                HStack {
+                    Text(story.title)
+                        .font(.system(size: 12, weight: .regular))
+                        .foregroundStyle(.white)
+                        .lineLimit(3)
+                        .multilineTextAlignment(.leading)
+                        .padding(.horizontal, 8)
+                        .padding(.bottom, 12)
+                    Spacer()
+                }
             }
-            Text(story.title)
-                .font(.system(size: 12, weight: .regular))
-                .foregroundStyle(.yUniversalWhite)
-                .lineLimit(3)
+            .frame(idealWidth: width, idealHeight: height)
         }
     }
 }
